b3efd3723c41d96f6fa4da25de2371d85c8e80f0
Tribhuwan <mathincosmos@gmail.com>
2023-06-26 22:17:53 +0530

    create schema for Items & category

--- models/item.js
+++ models/item.js
@@ -1,3 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const ItemSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
+  price: { type: Number, required: true },
+  stocks_count: { type: Number, required: true },
+  addOn: { type: Date, default: Date.now },
+});
+
+ItemSchema.virtual("url").get(function () {
+  return `/catalog/item/${this._id}`;
+});
+
+module.exports = mongoose.model("Category", ItemSchema);
